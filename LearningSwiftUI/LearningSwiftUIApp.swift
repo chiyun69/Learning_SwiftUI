88b615a3af00c87aa2fdddcc24b8af13c088e006
czy <chiyun69@hotmail.com>
2025-05-14 20:50:01 -0500

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Chi Zhuang Yan on 14/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningSwiftUITests/LearningSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  LearningSwiftUITests.swift
+//  LearningSwiftUITests
+//
+//  Created by Chi Zhuang Yan on 14/5/25.
+//
+
+import Testing
+
+struct LearningSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
